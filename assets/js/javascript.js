99b9526c888dcf26965d34937ed9631d04a8ce12
Matheus Rodrigues <Matheusrodd17@gmail.com>
2025-01-17 16:12:52 -0300

    realizado alterações nos css e index

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const words = ["Amante de café", "Desenvolvedor", "Analista de dados", "Solucionador de problemas"];
+    // Troca de palavras no título
+    const words = ["Coffee Lover", "Developer", "Data Analyst", "Problem Solver"];
     let index = 0;
     const title = document.getElementById("about-me-title");
 
@@ -13,4 +14,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Troca as palavras a cada 2 segundos (2000ms)
     setInterval(changeText, 2000);
+
+    // Rolagem suave para os links
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();  // Previne o comportamento padrão de navegação
+
+            const targetId = link.getAttribute('href').substring(1);  // Remove o '#' do href
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,  // A posição do topo do elemento
+                behavior: 'smooth'  // Rolagem suave
+            });
+        });
+    });
+
+    // Selecionando todos os itens de tecnologia (li)
+    const techItems = document.querySelectorAll("#technologies ul li");
+
+    techItems.forEach(item => {
+        // Adicionando o evento de mouseover
+        item.addEventListener("mouseenter", function () {
+            // Adiciona a classe 'bright' ao passar o mouse
+            item.classList.add("bright");
+        });
+
+        // Adicionando o evento de mouseout
+        item.addEventListener("mouseleave", function () {
+            // Remove a classe 'bright' quando o mouse sai
+            item.classList.remove("bright");
+        });
+    });
 });
